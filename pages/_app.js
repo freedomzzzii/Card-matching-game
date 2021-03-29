a16a270852d6b506e3fb709bb415556c05632607
freedomzzzii <theeraporn.tia@gmail.com>
2021-03-29 23:44:38 +0700

    feat: intigate redux to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
-// import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux';
 
-import '../styles/globals.css'
+import '../styles/globals.css';
+
+import store from '../redux';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store()}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
