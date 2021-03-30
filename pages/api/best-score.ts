8a7fc35daef1345f3b9c00b8476d9757efabfe69
freedomzzzii <theeraporn.tia@gmail.com>
2021-03-31 03:18:24 +0700

    feat: update api connect to firebase

--- pages/api/best-score.ts
+++ pages/api/best-score.ts
@@ -1,9 +1,36 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+import firebase from 'firebase';
+
 type responseBestScoreType = {
   score: number,
-}
-
-export default (req: NextApiRequest, res: NextApiResponse<responseBestScoreType>) => {
-  res.status(200).json({ score: 20 })
 };
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<responseBestScoreType>) {
+  try {
+    const bestScoreRef = firebase.database().ref('best-score');
+
+    if (req.method === 'POST') {
+      bestScoreRef.update(req.body);
+
+      bestScoreRef.once('value', (snapshot) => {
+        const data = snapshot.val();
+
+        res.status(200).json(data);
+      });
+    } else if (req.method === 'GET') {
+      bestScoreRef.once('value', (snapshot) => {
+        const data = snapshot.val();
+
+        res.status(200).json(data);
+      });
+    }
+  } catch (error) { // for don't want to connect firebase
+    if (req.method === 'POST') {
+      res.status(200).json(req.body);
+    } else if (req.method === 'GET') {
+      res.status(200).json({ score: 999 });
+    }
+    return;
+  }
+}
